773b11917cb659cd7bc464b18095813e1b6514e6
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-08 17:24:19 +0200

    removed support for unindexed meshes

--- src/mesh.rs
+++ src/mesh.rs
@@ -13,8 +13,8 @@ use crate::vertex_data;
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
 pub struct Vertex {
-    pub position: [f32;4],
-    pub color: [f32;4],
+    pub position: [f32; 4],
+    pub color: [f32; 4],
 }
 
 impl Vertex {
@@ -30,32 +30,16 @@ impl Vertex {
 
 pub struct Mesh {
     pub transform: Transform,
-    pub positions: Vec<[f32;3]>,
-    pub colors: Vec<[f32;3]>,
-    pub indices: Option<Vec<u32>>,
-    pub index_buffer: Option<Buffer>,
+    pub positions: Vec<[f32; 3]>,
+    pub colors: Vec<[f32; 3]>,
+    pub indices: Vec<u32>,
+    pub index_buffer: Buffer,
     pub vertex_buffer: Buffer,
     pub material: Rc<Material>,
 }
 
 impl Mesh {
-    pub fn new(engine: &mut Engine) -> Mesh {
-        Mesh {
-            transform: Transform::new(),
-            indices: None,
-            index_buffer: None,
-            positions: Vec::new(),
-            colors: Vec::new(),
-            vertex_buffer: engine.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: &[],
-                usage: wgpu::BufferUsages::VERTEX,
-            }),
-            material: Rc::new(Material::new(engine))
-        }
-    }
-
-    pub fn from_data(indices: Vec<u32>, positions: Vec<[f32;3]>, engine: &mut Engine) -> Mesh {
+    pub fn from_vertex_data(indices: Vec<u32>, positions: Vec<[f32; 3]>, engine: &mut Engine) -> Mesh {
         let mut colors = Vec::new();
         for _ in 0..positions.len() {
             colors.push([0.2, 0.2, 0.2]);
@@ -74,53 +58,29 @@ impl Mesh {
         Mesh {
             transform: Transform::new(),
             positions,
-            indices: Some(indices),
-            index_buffer: Some(index_buffer),
+            indices: indices,
+            index_buffer: index_buffer,
             colors,
             vertex_buffer,
-            material: Rc::new(Material::new(engine))
-        }
-    }
-
-    pub fn new_cube(engine: &mut Engine) -> Mesh {
-        let positions = vertex_data::cube_positions();
-        let colors = vertex_data::cube_colors();
-        let vertex_buffer = engine.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: cast_slice(&zip_vertex_data(&positions, &colors)),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-        Mesh {
-            transform: Transform::new(),
-            positions,
-            indices: None,
-            index_buffer: None,
-            colors,
-            vertex_buffer,
-            material: Rc::new(Material::new(engine))
+            material: Rc::new(Material::new(engine)),
         }
     }
 
     pub fn draw<'a, 'b>(&'a self, render_pass: &'b mut RenderPass<'a>) -> () {
         self.material.bind(render_pass);
 
-        if let Some(index_buffer) = &self.index_buffer {
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint32);
-            render_pass.draw_indexed(0..self.indices.as_ref().unwrap().len() as u32, 0, 0..1);
-        } else {
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.positions.len() as u32, 0..1);
-        }
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+        render_pass.draw_indexed(0..self.indices.len() as u32, 0, 0..1);
     }
 }
 
-pub fn zip_vertex_data(positions: &Vec<[f32;3]>, colors: &Vec<[f32;3]>) -> Vec<Vertex> {
+pub fn zip_vertex_data(positions: &Vec<[f32; 3]>, colors: &Vec<[f32; 3]>) -> Vec<Vertex> {
     let mut data: Vec<Vertex> = Vec::with_capacity(positions.len());
     for i in 0..positions.len() {
         data.push(Vertex {
             position: [positions[i][0], positions[i][1], positions[i][2], 1.0],
-            color: [colors[i][0], colors[i][1], colors[i][2], 1.0]
+            color: [colors[i][0], colors[i][1], colors[i][2], 1.0],
         });
     }
     data.to_vec()
