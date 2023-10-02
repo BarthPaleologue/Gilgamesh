38c8125f2c35f1f01771bb148a789eb362979f1b
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-02 17:18:36 +0200

    working on phong material

--- src/materials.rs
+++ src/materials.rs
@@ -1,3 +1,4 @@
 pub mod material_pipeline;
 pub mod phong;
 pub mod uniform_buffer;
+pub mod material;
--- src/materials/phong.rs
+++ src/materials/phong.rs
@@ -1,3 +1,8 @@
+use crate::core::wgpu_context::WGPUContext;
+use crate::materials::material_pipeline::MaterialPipeline;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct PhongUniforms {
     diffuse_color: [f32; 3],
     _padding1: u32,
@@ -5,4 +10,31 @@ pub struct PhongUniforms {
     _padding2: u32,
     specular_color: [f32; 3],
     _padding3: u32,
+}
+
+impl Default for PhongUniforms {
+    fn default() -> Self {
+        PhongUniforms {
+            diffuse_color: [1.0, 1.0, 1.0],
+            _padding1: 0,
+            ambient_color: [0.0, 0.0, 0.0],
+            _padding2: 0,
+            specular_color: [0.0, 0.0, 0.0],
+            _padding3: 0,
+        }
+    }
+}
+
+pub struct PhongMaterial {
+    pub phong_uniforms: PhongUniforms,
+    material_pipeline: MaterialPipeline,
+}
+
+impl PhongMaterial {
+    fn new(wgpu_context: &mut WGPUContext) -> Self {
+        PhongMaterial {
+            phong_uniforms: PhongUniforms::default(),
+            material_pipeline: MaterialPipeline::new_default(wgpu_context),
+        }
+    }
 }
